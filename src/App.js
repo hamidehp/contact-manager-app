86c3100917f879d9ebc386d72426daeefae3bba8
hamidehp <hamideh_p@yahoo.com>
2022-08-18 21:02:20 +0430

    route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
 import Teachers from './components/teacher/Teachers';
-//import {AddTeacher,Teachers,Teacher,EditTeacher,ViewTeacher,Navbar} from './components'
+import Teacher from './components/teacher/Teacher';
+import AddTeacher from './components/teacher/AddTeacher';
+import EditTeacher from './components/teacher/EditTeacher';
+import {Route,Routes,Navigate,useNavigate} from 'react-router-dom';
+//import {AddTeacher,Teachers,Teacher,EditTeacher,ViewTeacher} from './components/teacher'
 const App=()=> {
   const[getTeachers,setTeachers]=useState([]);
   const [Loading,setLoading]=useState(false);
@@ -11,9 +15,16 @@ const App=()=> {
     <div className="App">
       
   <Navbar />
- 
-  <Teachers Teachers={getTeachers} Loading={Loading} />
   
+  <Routes>
+      <Route path="/" element={<Navigate to="/teachers" />} />
+      <Route path="/teachers" element={<Teachers  Teachers={getTeachers} Loading={Loading}/>} />
+      <Route path="/teachers/add" element={<AddTeacher/>} />
+      <Route path="/teachers/:teacherId" element={<Teacher/>} />
+      <Route path="/teachers/edit/:teacherId" element={<EditTeacher/>} />
+
+    </Routes>
+    
     </div>
   );
 }
--- src/components/teacher/index.jsx
+++ src/components/teacher/index.jsx
@@ -1,9 +1,9 @@
-export {default as AddTeacher} from "./teacher/AddTeacher";
-export {default as EditTeacher} from "./teacher/EditTeacher";
-export {default as ViewTeacher} from "./teacher/ViewTeacher";
-export {default as Teacher} from "./teacher/Teacher";
-export {default as Teachers} from "./teacher/Teachers";
-export {default as SearchTeacher} from "./teacher/SearchTeacher";
+ export {default as AddTeacher} from "./teacher/AddTeacher";
+ export {default as EditTeacher} from "./teacher/EditTeacher";
+ export {default as ViewTeacher} from "./teacher/ViewTeacher";
+ export {default as Teacher} from "./teacher/Teacher";
+ export {default as Teachers} from "./teacher/Teachers";
+ export {default as SearchTeacher} from "./teacher/SearchTeacher";
 
-export {default as Navbar} from "./Navbar";
-export {default as Spinner} from "./Spinner";
+ export {default as Navbar} from "./Navbar";
+ export {default as Spinner} from "./Spinner";
